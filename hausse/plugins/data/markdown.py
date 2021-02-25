8dbddbfc0dc7810c0e6fbec859669e18fadfe3c0
André Nasturas <andrenasturas@git.delfosia.net>
2021-02-25 15:17:02 +0100

    Use default extras at call instead of init

--- hausse/plugins/data/markdown.py
+++ hausse/plugins/data/markdown.py
@@ -20,15 +20,17 @@ class Markdown(Plugin):
         Extras settings passed to the markdown processor.
     """
 
+    default_extras = ['cuddled-lists', 'fenced-code-blocks', 'footnotes', 'header-ids', 'markdown-in-html', 'metadata', 'noreferrer', 'tag-friendly', 'task_list']
+
     def __init__(self, selection = None, extras: List[str] = None):
 
         self.selection = Selector(selection) if selection is not None else ExtensionSelector("md", "markdown")
-        self.extras = extras if extras is not None else ['cuddled-lists', 'fenced-code-blocks', 'footnotes', 'header-ids', 'markdown-in-html', 'metadata', 'noreferrer', 'tag-friendly', 'task_list']
+        self.extras = extras
 
 
     def __call__(self, elements: List[Element], metadata: dict, settings: dict):
 
         for element in self.selection(elements, metadata, settings):
-            m = markdown(element._contents, extras=self.extras)
+            m = markdown(element._contents, extras=self.extras or self.default_extras)
             element._contents = str(m)
             element._update_metadata(m.metadata)
