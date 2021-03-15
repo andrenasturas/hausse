a0bb884e2de0ec51ddb0a06e0282b15f4b7aa413
André Nasturas <andrenasturas@git.delfosia.net>
2021-03-15 12:42:24 +0100

    :sparkles: Introduce module main with hausse.json autoload

--- hausse/__main__.py
+++ hausse/__main__.py
@@ -1,2 +1,25 @@
+import argparse
+from pathlib import Path
 
+from .utils import Defaults, Keys
+from .hausse import Hausse
+
+parser = argparse.ArgumentParser(prog="hausse", description="Modulable static project builder")
+
+parser.add_argument("Path", type=str, nargs="?", help="Project folder or hausse.json file path")
+parser.add_argument("-b", "--build", action="store_true", help="Builds the project")
+
+parser.parse_args()
+
+path = Path(parser.Path or ".")
+
+project = Hausse(path)
+
+if path.is_file and path.exists():
+    project.load(path)
+else:
+    project.load()
+
+if parser.build:
+    project.build()
 
